7620675ea57003e35d2eea29777bfb6a54b87d97
Sam Trenholme <strenholme.usenet@gmail.com>
2020-03-30 08:32:30 -0700

    Added script to show doubling time

---
+++ doubling.time.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+echo Date '     ' Doubling time \(days\)
+grep 'San Diego' data.csv | awk -F, '
+{
+	v = 7; # Get the average growth for the last seven days
+	n = n+1;
+	this = $5;
+	if(last>0) {
+		growth=this/last;
+		list[n%v]=growth
+	}
+	sum = 0;
+	for(a=0;a<v;a++) {
+		sum += list[a]
+	}
+	if(log(sum/v) > 0 && this>10) {
+		print $1 " " log(2)/log(sum/v)
+	}
+	last = this
+}'
