de69c736fb2963b8f8acd9d30fc127673b251b04
Sam Trenholme <strenholme.usenet@gmail.com>
2020-07-03 17:53:53 -0700

    Begin work on code to make SVG images of data

---
+++ stateNameAbbr.lua
@@ -0,0 +1,62 @@
+rem = {}
+rem = { #rem, -- This script is a Python and Lua polyglot.  It is to be
+        #rem, -- included in a Python or Lua script to create
+        #rem, -- the dictionary/table stateNameAbbr, which given
+        #rem, -- the name of the state (e.g. California), gives its
+        #rem, -- 2-letter upper case abbreviation (e.g. CA)
+        #rem, --
+        #rem, -- This script is by Sam Trenholme, and has been donated
+        #rem  -- to the public domain in 2020
+}
+
+stateNameAbbr = {}
+stateNameAbbr["Alabama"] = "AL"
+stateNameAbbr["Alaska"] = "AK"
+stateNameAbbr["Arizona"] = "AZ"
+stateNameAbbr["Arkansas"] = "AR"
+stateNameAbbr["California"] = "CA"
+stateNameAbbr["Colorado"] = "CO"
+stateNameAbbr["Connecticut"] = "CT"
+stateNameAbbr["Delaware"] = "DE"
+stateNameAbbr["Florida"] = "FL"
+stateNameAbbr["Georgia"] = "GA"
+stateNameAbbr["Hawaii"] = "HI"
+stateNameAbbr["Idaho"] = "ID"
+stateNameAbbr["Illinois"] = "IL"
+stateNameAbbr["Indiana"] = "IN"
+stateNameAbbr["Iowa"] = "IA"
+stateNameAbbr["Kansas"] = "KS"
+stateNameAbbr["Kentucky"] = "KY"
+stateNameAbbr["Louisiana"] = "LA"
+stateNameAbbr["Maine"] = "ME"
+stateNameAbbr["Maryland"] = "MD"
+stateNameAbbr["Massachusetts"] = "MA"
+stateNameAbbr["Michigan"] = "MI"
+stateNameAbbr["Minnesota"] = "MN"
+stateNameAbbr["Mississippi"] = "MS"
+stateNameAbbr["Missouri"] = "MO"
+stateNameAbbr["Montana"] = "MT"
+stateNameAbbr["Nebraska"] = "NE"
+stateNameAbbr["Nevada"] = "NV"
+stateNameAbbr["New Hampshire"] = "NH"
+stateNameAbbr["New Jersey"] = "NJ"
+stateNameAbbr["New Mexico"] = "NM"
+stateNameAbbr["New York"] = "NY"
+stateNameAbbr["North Carolina"] = "NC"
+stateNameAbbr["North Dakota"] = "ND"
+stateNameAbbr["Ohio"] = "OH"
+stateNameAbbr["Oklahoma"] = "OK"
+stateNameAbbr["Oregon"] = "OR"
+stateNameAbbr["Pennsylvania"] = "PA"
+stateNameAbbr["Rhode Island"] = "RI"
+stateNameAbbr["South Carolina"] = "SC"
+stateNameAbbr["South Dakota"] = "SD"
+stateNameAbbr["Tennessee"] = "TN"
+stateNameAbbr["Texas"] = "TX"
+stateNameAbbr["Utah"] = "UT"
+stateNameAbbr["Vermont"] = "VT"
+stateNameAbbr["Virginia"] = "VA"
+stateNameAbbr["Washington"] = "WA"
+stateNameAbbr["West Virginia"] = "WV"
+stateNameAbbr["Wisconsin"] = "WI"
+stateNameAbbr["Wyoming"] = "WY"
