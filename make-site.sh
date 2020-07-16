eaad309814e8711de0f8ccbb99b712edaf1d68d7
Sam Trenholme <strenholme.usenet@gmail.com>
2020-07-16 05:29:34 -0700

    Add make-site.sh

---
+++ make-site.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# This script makes the website and puts it in the GNUplot/ directory
+lua examine-growth.lua website
+cd GNUplot/
+gnuplot maps.gnuplot
+rm -f *gnuplot *csv
+cd ..
+
+# Anything below this line is optional
+
+# ImageMagick needed to run the following 
+convert -depth 8 GNUplot/hotSpots.svg GNUplot/hotSpots.png
+cat GNUplot/index.html | sed 's/hotSpots.svg/hotSpots.png/g' > foo
+if [ -e GNUplot/hotSpots.png then ]
+  mv foo GNUplot/index.html
+fi
+
+# AdvanceComp to make main PNG files smaller
+cd GNUplot/
+advpng -z -4 hotSpots.png USA.png USA-deaths.png
+ls *png | grep -v , | awk '{print "advpng -z -3 \"" $0 "\""}' | sh
+
