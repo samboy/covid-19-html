5162a0109a68dd25cb5f94fd4099fa50940d4427
Sam Trenholme <strenholme.usenet@gmail.com>
2020-07-16 05:41:15 -0700

    make-site: Fix bugs, wrap up

--- make-site.sh
+++ make-site.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 # This script makes the website and puts it in the GNUplot/ directory
+# The total execution time is 6.5 minutes on my i7-7600U
+
 lua examine-growth.lua website
 cd GNUplot/
 gnuplot maps.gnuplot
@@ -11,7 +13,7 @@ cd ..
 # ImageMagick needed to run the following 
 convert -depth 8 GNUplot/hotSpots.svg GNUplot/hotSpots.png
 cat GNUplot/index.html | sed 's/hotSpots.svg/hotSpots.png/g' > foo
-if [ -e GNUplot/hotSpots.png then ]
+if [ -e GNUplot/hotSpots.png ] ; then 
   mv foo GNUplot/index.html
 fi
 
