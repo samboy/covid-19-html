110290a0823862fee8189f9a82220c8b082fa3d1
Sam Trenholme <maradns@gmail.com>
2020-07-26 01:16:49 -0400

    covid-19-byCounty.json updated; make-site.sh CentOS8 adaptations

--- make-site.sh
+++ make-site.sh
@@ -2,7 +2,13 @@
 # This script makes the website and puts it in the GNUplot/ directory
 # The total execution time is 6.5 minutes on my i7-7600U
 
-lua examine-growth.lua website
+LUA=lua
+# This is a custom 32-bit compile of Lunacy 
+if [ -e /usr/local/bin/lunacy32 ] ; then
+	LUA=/usr/local/bin/lunacy32
+fi
+$LUA examine-growth.lua website
+
 cd GNUplot/
 gnuplot maps.gnuplot
 rm -f *gnuplot *csv
@@ -10,8 +16,11 @@ cd ..
 
 # Anything below this line is optional
 
-# ImageMagick needed to run the following 
-convert -depth 8 GNUplot/hotSpots.svg GNUplot/hotSpots.png
+# ImageMagick or Inkscape needed to do svg -> png conversion
+inkscape GNUplot/hotSpots.svg --export-png=GNUplot/hotSpots.png
+if [ ! -e GNUplot/hotSpots.png ] ; then 
+	convert -depth 8 GNUplot/hotSpots.svg GNUplot/hotSpots.png
+fi
 cat GNUplot/index.html | sed 's/hotSpots.svg/hotSpots.png/g' > foo
 if [ -e GNUplot/hotSpots.png ] ; then 
   mv foo GNUplot/index.html
